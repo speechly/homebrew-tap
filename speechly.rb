fa1b1fdc0ab7838b5db75c66e876dd691822dab5
goreleaserbot <goreleaser@carlosbecker.com>
2020-08-11 12:37:18 +0300

    Brew formula update for speechly version v0.1.1

--- speechly.rb
+++ speechly.rb
@@ -2,16 +2,16 @@
 class Speechly < Formula
   desc ""
   homepage "https://www.speechly.com/"
-  version "0.1.0"
+  version "0.1.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/speechly/cli/releases/download/v0.1.0/speechly_0.1.0_macOS_x86_64.tar.gz"
-    sha256 "013d2beb7cc1f2867c353c141e279129ab895862bca7c2db77ebb2ddc36bfa59"
+    url "https://github.com/speechly/cli/releases/download/v0.1.1/speechly_0.1.1_macOS_x86_64.tar.gz"
+    sha256 "59f7de68f2abb033b052babadeb7f9bf604e1f1198a2556019f8b752f1021b07"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/speechly/cli/releases/download/v0.1.0/speechly_0.1.0_Linux_x86_64.tar.gz"
-      sha256 "c0d1bf4eced585fa9b8e12284f5b93fcf0bcd165717c40dc5c59bcca821270c1"
+      url "https://github.com/speechly/cli/releases/download/v0.1.1/speechly_0.1.1_Linux_x86_64.tar.gz"
+      sha256 "9c80f9e8c6d67da6276cdd9d332cf392441c02aae5733d8f86d9a6ea3a388d53"
     end
   end
 
