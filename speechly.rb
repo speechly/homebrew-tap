64f51a674212f4df52a27f6d1b47c76b84ec09e9
goreleaserbot <goreleaser@carlosbecker.com>
2021-02-12 09:44:08 +0200

    Brew formula update for speechly version v0.3.2

--- speechly.rb
+++ speechly.rb
@@ -5,16 +5,16 @@
 class Speechly < Formula
   desc ""
   homepage "https://www.speechly.com/"
-  version "0.3.1"
+  version "0.3.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/speechly/cli/releases/download/v0.3.1/speechly_0.3.1_macOS_x86_64.tar.gz"
-    sha256 "41a4fd5247a0e47b099b905028db07a35b3c779c42e53a758dd3a527906cfdbe"
+    url "https://github.com/speechly/cli/releases/download/v0.3.2/speechly_0.3.2_macOS_x86_64.tar.gz"
+    sha256 "d4f5d07797a89e4d446c6299c27c1b63e9b8159ab67fc20e1ec3a8273e03799d"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/speechly/cli/releases/download/v0.3.1/speechly_0.3.1_Linux_x86_64.tar.gz"
-    sha256 "74d640c8c895d9e95d933afe022ecb7ecc92dd47cab3f0ddc3e5f009d259885c"
+    url "https://github.com/speechly/cli/releases/download/v0.3.2/speechly_0.3.2_Linux_x86_64.tar.gz"
+    sha256 "d5578a33d95d905e47e22292abd2cfc88c6ddc334f26d7af8d63786f24d50bcf"
   end
 
   def install
